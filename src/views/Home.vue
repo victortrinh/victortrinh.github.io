8c82ee3d2dd6c7d2d1ea9d7a331a03767bec85a7
Victor Trinh <victor.trinh@outlook.com>
2021-06-11 17:33:34 -0400

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <img :src="image" />
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      image:
+        "https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/0001.jpg",
+    };
+  },
+};
+</script>
+
+<style scoped>
+img {
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  width: 100%;
+  height: auto;
+  transform: translate(-50%, -50%);
+}
+</style>
